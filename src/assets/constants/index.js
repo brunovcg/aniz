10312103191d7d15bd618e9851fbc5decb1a6050
Bruno Gouveia <brunovcg@gmail.com>
2022-02-15 20:34:02 -0300

    Alteração cardapio 15.2.22

--- src/assets/constants/index.js
+++ src/assets/constants/index.js
@@ -5,24 +5,14 @@ export const itens = [
     desc: "Filé de peito de frango, cortado em cubos com molho à base de mostarda e ketchup",
   },
   {
-    title: "Frango Grelhado",
+    title: "Frango grelhado",
     price: "19,90",
     desc: "Filé de peito de frango grelhado",
   },
   {
-    title: "Frango à Parmegiana",
+    title: "Frango à parmegiana",
     price: "21,90",
     desc: "Filé de peito de frango empanado coberto com delicioso molho de tomate",
-  },
-  {
-    title: "Frango à Cubana",
-    price: "22,90",
-    desc: "Frango empanado",
-  },
-  {
-    title: "Frango Thai",
-    price: "22,90",
-    desc: "Frango em cubos com delicioso molho de curry",
   },
   {
     title: "Frango com molho de mostarda",
@@ -30,14 +20,9 @@ export const itens = [
     desc: "Filé de peito de frango grelhado acompanhado por delicioso molho de mostarda",
   },
   {
-    title: "Filé de Tilápia grelhada",
+    title: "Filé de tilápia grelhada",
     price: "31,90",
     desc: "Filé de tilápia grelhado",
-  },
-  {
-    title: "Filé de Tilápia grelhada de alcaparras",
-    price: "31,90",
-    desc: "Filé de tilápia grelhado com delicioso molho de alcaparras e azeite",
   },
   {
     title: "Filé a parmegiana",
@@ -55,19 +40,14 @@ export const itens = [
     desc: "Maminha grelhada na chapa",
   },
   {
-    title: "Filé ao vinho",
-    price: "31,90",
-    desc: "Filé grelhado com molho a base de vinho e creme de leite",
-  },
-  {
-    title: "Filé ao molho madeira",
-    price: "32,90",
-    desc: "Filé grelhado com molho a madeira",
-  },
-  {
     title: "Carne de sol do sertão",
     price: "29,90",
     desc: "Cubos de Carne de Sol acebolada",
+  },
+  {
+    title: "Espaguete á bolonhesa",
+    price: "26,90",
+    desc: "Massa ao molho de tomate e carne moída",
   },
 ];
 
@@ -77,16 +57,19 @@ export const sides = [
     desc: "Mix de folhas, cenoura ralada, repolho roxo, tomatinhos",
   },
   {
-    title: "Macarrão talharim",
+    title: "Macarrão espaguete na manteiga",
   },
   {
-    title: "Pure de batata",
+    title: "Purê de batata",
   },
   {
-    title: "Batata Frita",
+    title: "Batata frita",
   },
   {
     title: "Arroz Branco",
+  },
+  {
+    title: "Arroz Integral",
   },
   {
     title: "Arroz de Cenoura",
@@ -94,87 +77,103 @@ export const sides = [
   {
     title: "Feijão Verde",
   },
+  {
+    title: "Feijão Carioca",
+  },
+  {
+    title: "Vinagrete e Farofa",
+  },
 ];
 
 export const foodies = [
-{
-  title: "Omelete de queijo mussarela, tomate e orégano",
-  price: "12,90"
-},
-{
-  title: "Omelete misto de queijo e presunto",
-  price: "12,90"
-},
-{
-  title: "Omelete de frango",
-  price: "13,90"
-},
-{
-  title: "Tapioca de queijo coalho",
-  price: "8,90"
-},
-{
-  title: "Tapioca de Coco e queijo coalho",
-  price: "9,90"
-},
-{
-  title: "Tapioca de Frango com queijo coalho",
-  price: "10,90"
-},
-{
-  title: "Cuscuz com Charque e queijo",
-  price: "10,90"
-},
-{
-  title: "Cuscuz simples apenas com manteiga derretida",
-  price: "7,90"
-},
-{
-  title: "Cuscuz com charque",
-  price: "12,90"
-},
-{
-  title: "Cuscuz com ovo",
-  price: "10,90"
-},
-{
-  title: "Cuscuz a pizzaiolo com molho de tomate, queijo e orégano",
-  price: "12,90"
-},
-{
-  title: "Sanduíche misto de pão baguete",
-  price: "12,90"
-},
-{
-  title: "Sanduíche de queijo e salada em pão baguete",
-  price: "12,90"
-},
-{
-  title: "Sanduíche de frango com queijo e salada em pão baguete",
-  price: "15,90"
-},
-{
-  title: "Croissant Misto",
-  price: "7,90"
-},
-{
-  title: "Croissant de Frango",
-  price: "8,90"
-}
-
-]
+  {
+    title: "Omelete de queijo mussarela, tomate e orégano",
+    price: "12,90",
+  },
+  {
+    title: "Omelete misto de queijo e presunto",
+    price: "12,90",
+  },
+  {
+    title: "Omelete de frango",
+    price: "13,90",
+  },
+  {
+    title: "Tapioca de queijo coalho",
+    price: "8,90",
+  },
+  {
+    title: "Tapioca de Coco e queijo coalho",
+    price: "9,90",
+  },
+  {
+    title: "Tapioca de Frango com queijo coalho",
+    price: "10,90",
+  },
+  {
+    title: "Cuscuz com Charque e queijo",
+    price: "10,90",
+  },
+  {
+    title: "Cuscuz simples apenas com manteiga derretida",
+    price: "7,90",
+  },
+  {
+    title: "Cuscuz com charque",
+    price: "12,90",
+  },
+  {
+    title: "Cuscuz com ovo",
+    price: "10,90",
+  },
+  {
+    title: "Cuscuz a pizzaiolo com molho de tomate, queijo e orégano",
+    price: "12,90",
+  },
+  {
+    title: "Sanduíche misto de pão baguete",
+    price: "12,90",
+  },
+  {
+    title: "Sanduíche de queijo e salada em pão baguete",
+    price: "12,90",
+  },
+  {
+    title: "Sanduíche de frango com queijo e salada em pão baguete",
+    price: "15,90",
+  },
+  {
+    title: "Croissant Misto",
+    price: "7,90",
+  },
+  {
+    title: "Croissant de Frango",
+    price: "8,90",
+  },
+];
 
 export const saladas = [
   {
     title: "Salada de Frango",
-    desc: "Salada com mix de folhas, repolho roxo, cenoura ralada e tomate cereja acompanhada de peito de frango grelhado.",
-    price: "28,90"
+    price: "23,90",
   },
   {
     title: "Salada de Camarão",
-    desc: "Salada com mix de folhas, repolho roxo, cenoura ralada e tomate cereja acompanhada de camarões grelhados.",
-    price: "32,90"
+    price: "27,90",
   },
+];
 
-  
-]
+export const drinks = [
+  {
+    title: "Água (500ml)",
+    price: "3,00",
+  },
+  {
+    title: "Suco de Laranja Natural (500ml)",
+    price: "6,90",
+  },
+  {
+    title: "Refrigerante (Lata)",
+    price: "5,90",
+  },
+];
--- src/views/Menu/index.jsx
+++ src/views/Menu/index.jsx
@@ -1,70 +1,105 @@
 import Styled from "./styles";
-import logo from "../../assets/images/logo.png"
+import logo from "../../assets/images/logo.png";
 import { FaInstagram } from "react-icons/fa";
 
-import {itens, sides, foodies, saladas} from "../../assets/constants"
+import { itens, sides, foodies, saladas, drinks } from "../../assets/constants";
 
 const Menu = () => {
- 
-
   return (
     <Styled>
       <div className="transparent">
         <div className="logo-insta">
-        <figure className="logo-box">
-          <img src={logo} alt="logo" />
-        </figure>
-        <div className="icon">
-          <a href="https://instagram.com/anizcozinha?utm_medium=copy_link"><FaInstagram/><span>{"      "}{ "    anizcozinha"}</span></a>
-          
-        </div>
+          <figure className="logo-box">
+            <img src={logo} alt="logo" />
+          </figure>
+          <div className="icon">
+            <a href="https://instagram.com/anizcozinha?utm_medium=copy_link">
+              <FaInstagram />
+              <span>
+                {"      "}
+                {"    anizcozinha"}
+              </span>
+            </a>
+          </div>
         </div>
         <div className="box">
           <h2>Almoço</h2>
+          <p style={{ color: "white", marginTop: "10px", fontSize: "18px" }}>
+            *Escolha 2 acompanhamentos{" "}
+            <span style={{ fontSize: "12px" }}>
+              (exceto para o Espaguete à bolonhesa)
+            </span>
+          </p>
           <section>
             {itens &&
-              itens.map((item, index) => <article key={index}> 
-              <div className="title-price">
-                <div className="title">{item.title}</div>
-                <div className="price">R$ {item.price}</div>
-              </div>
-              <p className="desc">{item.desc}</p>
-              </article>)}
+              itens.map((item, index) => (
+                <article key={index}>
+                  <div className="title-price">
+                    <div className="title">{item.title}</div>
+                    <div className="price">R$ {item.price}</div>
+                  </div>
+                  <p className="desc">{item.desc}</p>
+                </article>
+              ))}
           </section>
           <h2>Acompanhamentos</h2>
           <section>
             {sides &&
-              sides.map((item, index) => <article key={index}> 
-              <div className="title-price">
-                <div>{item.title}</div>
-              </div>
-              {item.desc && <p className="desc">{item.desc}</p>}
-              </article>)}
+              sides.map((item, index) => (
+                <article key={index}>
+                  <div className="title-price">
+                    <div>{item.title}</div>
+                  </div>
+                  {item.desc && <p className="desc">{item.desc}</p>}
+                </article>
+              ))}
           </section>
           <h2>Saladas</h2>
+          <p style={{ color: "var(--light-green)", textAlign: "center",margin: "20px 20px 0px 20px"}}>
+            Salada com mix de folhas, repolho roxo, cenoura ralada, tomate
+            cereja e torradinhas. Acompanha proteína a sua escolha:
+          </p>
           <section>
             {saladas &&
-              saladas.map((item, index) => <article key={index}> 
-              <div className="title-price">
-                <div>{item.title}</div>
-                <div className="price">R$ {item.price}</div>
-              </div>
-              {item.desc && <p className="desc">{item.desc}</p>}
-              </article>)}
+              saladas.map((item, index) => (
+                <article key={index}>
+                  <div className="title-price">
+                    <div>{item.title}</div>
+                    <div className="price">R$ {item.price}</div>
+                  </div>
+                  {item.desc && <p className="desc">{item.desc}</p>}
+                </article>
+              ))}
           </section>
-          <h2>Comidinhas</h2>
+          <h2>Salgados</h2>
           <section>
             {foodies &&
-              foodies.map((item, index) => <article key={index}> 
-              <div className="title-price">
-                <div>{item.title}</div>
-                <div className="price">R$ {item.price}</div>
-              </div>
-              {item.desc && <p className="desc">{item.desc}</p>}
-              </article>)}
+              foodies.map((item, index) => (
+                <article key={index}>
+                  <div className="title-price">
+                    <div>{item.title}</div>
+                    <div className="price">R$ {item.price}</div>
+                  </div>
+                  {item.desc && <p className="desc">{item.desc}</p>}
+                </article>
+              ))}
+
           </section>
 
+          <h2>Bebidas</h2>
+          <section>
+            {drinks &&
+              drinks.map((item, index) => (
+                <article key={index}>
+                  <div className="title-price">
+                    <div>{item.title}</div>
+                    <div className="price">R$ {item.price}</div>
+                  </div>
+                  {item.desc && <p className="desc">{item.desc}</p>}
+                </article>
+              ))}
 
+          </section>
         </div>
       </div>
     </Styled>
